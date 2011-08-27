e4bde9de4507e720197197fca58a5746d69f0563
Andrea Righi <andrea@betterlinux.com>
2011-08-27 16:36:58 +0200

    add a script to build the kernel: build_kernel.sh
    Signed-off-by: Andrea Righi <andrea@betterlinux.com>

---
+++ build_kernel.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TOPDIR=`pwd`
+OUTPUT=/tmp/galaxy-i9100-kernel-gingerbread-arighi.tar
+CONFIG=$TOPDIR/.config
+TMP_INITRAMFS=/tmp/initramfs
+MAKE_OPTS=
+
+# configure
+if [ ! -e $CONFIG ]; then
+	make ARCH=arm CROSS_COMPILE=arm-none-eabi- c1_rev02_defconfig
+fi
+
+# make modules
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- modules_prepare
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- modules
+
+# generate initramfs
+if [ -e ../initramfs/ ]; then
+	rm -rf $TMP_INITRAMFS $TMP_INITRAMFS.cpio
+
+	cp -af ../initramfs/ $TMP_INITRAMFS
+	find -name '*.ko' -exec cp -av {} $TMP_INITRAMFS/lib/modules/ \;
+
+	cd $TMP_INITRAMFS
+	find | fakeroot cpio -H newc -o > $TMP_INITRAMFS.cpio 2>/dev/null
+	ls -lh $TMP_INITRAMFS.cpio
+	cd -
+
+	MAKE_OPTS="$MAKE_OPTS CONFIG_INITRAMFS_SOURCE=$TMP_INITRAMFS.cpio"
+fi
+
+# make kernel
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- $MAKE_OPTS
+
+# prepare kernel to be flashed by Odin
+cd arch/arm/boot
+tar cf $OUTPUT zImage && ls -lh $OUTPUT
