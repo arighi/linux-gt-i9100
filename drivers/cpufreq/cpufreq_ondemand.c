ee69f4c39f61a94b6ff0dcd3ef9172e527958048
Andrea Righi <andrea@betterlinux.com>
2011-10-07 22:50:22 +0200

    cpufreq: ondemand: apply ondemand behavior at early stage of boot
    Do not start to apply the ondemand frequency regulation only after 40sec
    after the boot, ondemand must work fine even at the early stage of boot.
    
    Also, don't care too much about synchronizing the workqueue in both
    cpus.
    
    Signed-off-by: Andrea Righi <andrea@betterlinux.com>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -762,13 +762,7 @@ static void do_dbs_timer(struct work_struct *work)
 
 static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 {
-	/* We want all CPUs to do sampling nearly on same jiffy */
 	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate);
-#if 0
-	delay -= jiffies % delay;
-#else
-	delay = usecs_to_jiffies(40 * 1000000);
-#endif
 
 	dbs_info->sample_type = DBS_NORMAL_SAMPLE;
 	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
